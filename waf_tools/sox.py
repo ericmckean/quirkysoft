0ade4b3765917d375aab503179480df3e35cefae
Richard Quirk <richard.quirk@gmail.com>
2008-07-22 21:22:23 +0200

    Fix for Ubuntu 8.04 sox stupidity

--- waf_tools/sox.py
+++ waf_tools/sox.py
@@ -7,6 +7,23 @@ def detect(conf):
     flags = " -r 16000 -s -b -c 1 "
     if not conf.env['SOXFLAGS']:
       conf.env['SOXFLAGS'] = flags.split()
+    # check sox SUPPORTED FILE FORMATS has wav
+    # else: sudo apt-get install libsox-fmt-all
+    process_pipe = subprocess.Popen([sox, '-h'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process_pipe.wait()
+    have_wav = False
+    v = process_pipe.stdout.readlines()
+    v.extend(process_pipe.stderr.readlines())
+    for l in v:
+      if l.upper().startswith('SUPPORTED FILE FORMATS'):
+        if 'wav' in l.split(':')[1].split():
+          have_wav = True
+          break
+    conf.check_message(sox, 'has wav file format', have_wav)
+
+  if not have_wav:
+    Logs.warn("""sox does not have wav. run `sudo apt-get install libsox-fmt-all'?""")
+    conf.env['SOX'] = None
 
   dka_bin = '%s/bin' % (Params.g_options.devkitarm)
   padbin = conf.find_program('padbin', path_list=[dka_bin], var='PADBIN')
