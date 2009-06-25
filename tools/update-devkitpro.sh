98edc5616387aa95289664b2ab9b7e627697eab9
Richard Quirk <richard.quirk@gmail.com>
2009-06-25 21:28:34 +0200

    Add an update-devkitpro script

---
+++ tools/update-devkitpro.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+die() {
+    echo $@
+    exit 1
+}
+
+g() {
+    python <<EOF
+import ConfigParser
+c = ConfigParser.ConfigParser()
+c.readfp(open("$here/" + 'devkitProUpdate.ini'))
+print c.get("$1", "$2")
+EOF
+}
+
+if [ -z $DEVKITPRO ] ; then
+    die "Set DEVKITPRO"
+fi
+
+if [ ! -d $DEVKITPRO ] ; then
+    mkdir $DEVKITPRO
+fi
+cd $DEVKITPRO
+here=$(pwd)
+url="http://devkitpro.sourceforge.net/devkitProUpdate.ini"
+if [ ! -e devkitProUpdate.ini ] ; then
+    wget $url || die "Unable to download devkitProUpdate.ini"
+fi
+
+section=devkitProUpdate
+url=$(g $section URL)
+platform=$(uname -m)-linux.tar.bz2
+
+for item in devkitARM libnds libndsfat maxmodds dswifi filesystem defaultarm7 ndsexamples
+do
+    dl=$(g $item File | sed "s/win32\.exe/$platform/g")
+    v=$(g $item Version)
+    if [ -e $dl ] ; then
+        printf "Looks like you already have $item $v. Download and install anyway? [y/N]"
+        read doit
+        if [ "x$doit" != "xy" ] ; then
+            continue
+        fi
+    fi
+    wget $url/$dl
+
+    case $item in
+        devkitARM)
+            touch $item-$v
+            tar xf $dl
+            ;;
+        ndsexamples)
+            mkdir -p $item-$v
+            cd $item-$v
+            tar xf ../$dl
+            cd ..
+            ;;
+        *)
+            touch $item-$v
+            mkdir -p libnds
+            cd libnds
+            tar xf ../$dl
+            cd ..
+            ;;
+    esac
+done
