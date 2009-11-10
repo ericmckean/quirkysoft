a24d54d95385c5985122d1baa58361f51bbe2503
Richard Quirk <richard.quirk@gmail.com>
2009-11-10 21:56:58 +0100

    Add profiling tools

--- tools/test-time.sh
+++ tools/test-time.sh
@@ -5,7 +5,17 @@ count=$2
 
 let i=0
 while [[ $i -lt $count ]] ; do
-  /usr/bin/time -f %E $prog > /dev/null
+  /usr/bin/time -f %e $prog > /dev/null
   let i=$i+1
-done
+done 2>&1 | tee /tmp/times$$.log
 
+python <<EOF
+total = 0
+times = 0
+for line in open("/tmp/times$$.log"):
+    times += 1
+    total += float(line)
+print "Avg: " + str(total/times)
+EOF
+
+rm /tmp/times$$.log
